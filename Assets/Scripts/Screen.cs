3675ad16ea205349fb1af8af5d5577488c4e6c71
Brett Cundal <brett.cundal@iugome.com>
2019-01-25 23:17:35 -0800

    Add AirConsole lib and basic boop scene

---
+++ Assets/Scripts/Screen.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using NDream.AirConsole;
+using Newtonsoft.Json.Linq;
+
+public class Screen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    	AirConsole.instance.onMessage += OnMessage;
+    }
+
+	void OnMessage(int from, JToken data) {
+		Debug.Log("Got message");
+    	AirConsole.instance.Message(from, "Full of pixels!");
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
