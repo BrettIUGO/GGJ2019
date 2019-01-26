5002bf1fda77fc6e39346bf9cb8d8996e7a62925
Brett Cundal <brett.cundal@iugome.com>
2019-01-26 09:48:58 -0800

    Basic client init and pulse messages

--- Assets/Scripts/Screen.cs
+++ Assets/Scripts/Screen.cs
@@ -7,20 +7,43 @@ using Newtonsoft.Json.Linq;
 
 public class Screen : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-    	AirConsole.instance.onMessage += OnMessage;
-    }
-
-	void OnMessage(int from, JToken data) {
-		Debug.Log("Got message");
-    	AirConsole.instance.Message(from, "Full of pixels!");
+	// Start is called before the first frame update
+	void Start()
+	{
+		AirConsole.instance.onConnect += OnConnect;
+		AirConsole.instance.onDisconnect += OnDisconnect;
+		AirConsole.instance.onMessage += OnMessage;
 	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	void OnMessage(int deviceId, JToken data)
+	{
+		Debug.Log("Got message of type: " + data["type"] + " from " + deviceId);
+	}
+
+	void OnConnect(int deviceId)
+	{
+		Debug.Log("Instance connected: " + deviceId);
+
+		var message = new
+		{
+			type = "init",
+			data = JObject.FromObject(new
+			{
+				avatar = "avatar" + deviceId + ".png",
+				sequence = new JArray(1, 2, 3, 4, 5)
+			})
+		};
+		AirConsole.instance.Message(deviceId, message);
+	}
+
+	void OnDisconnect(int deviceId)
+	{
+		Debug.Log("Instance disconnected: " + deviceId);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
 }
