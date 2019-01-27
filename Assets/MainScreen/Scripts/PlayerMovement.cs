9ea10a832c24554670ff81e824e051caf3843283
james-battersby-ggj2019 <james.battersby@iugome.com>
2019-01-27 15:25:24 -0800

    An attempt at obstacle avoidance

--- Assets/MainScreen/Scripts/PlayerMovement.cs
+++ Assets/MainScreen/Scripts/PlayerMovement.cs
@@ -14,19 +14,49 @@ public class PlayerMovement : MonoBehaviour
     private Vector3 destination;
 
     private Rigidbody rb;
+    private bool touching;
+    private Vector3 avoidanceVector;
 
     private void Awake()
     {
         _moveStartTime = 0;
         rb = GetComponent<Rigidbody>();
+        touching = false;
     }
 
     public void SetDestination(Vector3 destination)
     {
-        if((transform.position - destination).magnitude > float.Epsilon)
+        Vector3 forceDirection = destination - transform.position;
+        if (forceDirection.magnitude > float.Epsilon)
         {
-            Vector3 force = (destination - transform.position).normalized * forceModifier;
+            forceDirection.Normalize();
+            if (touching)
+            {
+                forceDirection = Vector3.Cross(avoidanceVector, forceDirection);
+            }
+
+            Vector3 force = forceDirection * forceModifier;
             rb.AddForce(force, ForceMode.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.tag == "MapElement")
+        {
+            avoidanceVector = collision.relativeVelocity.normalized;
+            float x = avoidanceVector.x;
+            avoidanceVector.x = avoidanceVector.z;
+            avoidanceVector.z = -x;
+            touching = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.collider.tag == "MapElement")
+        {
+            touching = false;
         }
     }
 }
